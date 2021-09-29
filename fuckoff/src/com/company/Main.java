d314943ee01f0d7144a0efe900276b08dcb7cd1f
Victorkaffekande <90904666+Victorkaffekande@users.noreply.github.com>
2021-09-29 19:08:33 +0200

    to leet sp34|<

--- fuckoff/src/com/company/Main.java
+++ fuckoff/src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
         StringExercise stringExercise = new StringExercise();
 
         String input = "Test horse horse spores saddle test";
+        String palInput = "EvaCanIStab";
 
         //add tags
         System.out.println(stringExercise.addTags(input));
@@ -20,10 +21,19 @@ public class Main {
         System.out.println(stringExercise.reverseString(input));
 
         //to morse
-        String string = stringExercise.toMorse(input);
-        System.out.println(string);
+        System.out.println(stringExercise.toMorse(input));
 
         //censor String
-        System.out.println(StringExercise.censorString(input));
+        System.out.println(stringExercise.censorString(input));
+
+        //palindrome String
+        System.out.println(stringExercise.palindromeString(palInput));
+
+        // Alternate caps
+        System.out.println(stringExercise.alternateCaps(input));
+
+        //to leet
+        System.out.println(stringExercise.to1337(input));
+
     }
 }
--- fuckoff/src/com/company/StringExercise.java
+++ fuckoff/src/com/company/StringExercise.java
@@ -1,15 +1,13 @@
 package com.company;
 
-import java.util.Arrays;
-
 public class StringExercise {
     public String addTags(String input) {
         return "<" + input + ">";
     }
 
-    public String countChars(String input) {
+    public int countChars(String input) {
         int nrOfChars = input.length();
-        return nrOfChars + "";
+        return nrOfChars;
     }
 
     public String addAtString(String input) {
@@ -21,6 +19,7 @@ public class StringExercise {
         return new StringBuffer(input).reverse().toString();
 
     }
+
 
     public String toMorse(String input) {
         char[] english = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
@@ -51,7 +50,7 @@ public class StringExercise {
         return str;
     }
 
-    public static String censorString(String input) {
+    public String censorString(String input) {
         String[] censorList = {"horse", "spores", "saddle"};
         StringBuilder sb = new StringBuilder(input);
 
@@ -65,4 +64,60 @@ public class StringExercise {
         return sb.toString();
     }
 
+    public String palindromeString(String input) {
+        StringBuilder sb = new StringBuilder(input);
+        sb = sb.reverse();
+        String output = input + "-" + sb;
+
+        return output;
+    }
+
+    public String alternateCaps(String input) {
+        StringBuilder sb = new StringBuilder();
+        input = input.toLowerCase();
+        char[] chars = input.toCharArray();
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (count % 2 == 0) {
+                String str = chars[i] + "";
+                str = str.toUpperCase();
+                sb = sb.append(str);
+                if (str.isBlank()) {
+                    count++;
+                }
+            } else {
+                sb = sb.append(chars[i]);
+            }
+            count++;
+        }
+        return sb.toString();
+    }
+
+    public String to1337(String input) {
+        char[] english = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+                ',', '.', '?'};
+        String[] leet = {" ", "4", "|3", "(", "|)", "3", "|=", "6", "|-|", "|", "9", "|<", "1", "|v|", "|/|",
+                "0", "|*", "0,", "|2", "5", "7", "|_|", "|/", "|/|/", "><", "`/", "2"};
+
+        //force lowercase
+        String userInput = input.toLowerCase();
+
+        // make array out of input
+        char[] chars = userInput.toCharArray();
+
+        String str = "";
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < leet.length; j++) {
+
+                if (english[j] == chars[i]) {
+                    str = str + leet[j];
+                }
+            }
+        }
+        return str;
+    }
+
 }
+
